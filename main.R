dbf5920cfc25facde40cfae95400e438b979deb8
manika15 <manika.kapoor90@gmail.com>
2017-08-22 16:41:04 -0700

    add comments for better understanding

--- main.R
+++ main.R
@@ -5,8 +5,3 @@ library(utils)
 setwd("~/Documents/stanford/datascience/employee_attrition_prediction")
 data = read.csv("HR-Employee-Attrition.csv") 
 
-data_yes_attrition = data[!(data$Attrition=="No"),]
-library(ggplot2)
-ggplot(data,aes(data$MonthlyIncome,data$WorkLifeBalance, color=Attrition))+geom_point()
-
-ggplot(data, aes(x=Attrition, y=JobSatisfaction, fill=Attrition)) + geom_violin() + theme_bw()
