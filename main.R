dea0ed80cfb09109ed4a6e0b2ff151bd7e717ec0
manika15 <manika.kapoor90@gmail.com>
2017-08-22 16:46:51 -0700

    add scatter plot between work life balance and monthly income

--- main.R
+++ main.R
@@ -5,3 +5,8 @@ library(utils)
 setwd("~/Documents/stanford/datascience/employee_attrition_prediction")
 data = read.csv("HR-Employee-Attrition.csv") 
 
+#Try different plots
+library(ggplot2)
+
+#scatter plot between monthly income, work life balance and attrition
+ggplot(data,aes(data$MonthlyIncome,data$WorkLifeBalance, color=Attrition))+geom_point()
